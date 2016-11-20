09a4cc71efd8a9e4f6d455312a1ac48a15d990f9
sbfnk <sbfnk@users.noreply.github.com>
2016-11-20 00:44:25 +0000

    POLYMOD data description

---
+++ R/polymod.r
@@ -0,0 +1,18 @@
+#' Social contact data from 8 European countries
+#'
+#' A dataset containing social mixing diary data from 8 European countries:
+#' Belgium, Germany, Finland, Great Britain, Italy, Luxembourg,
+#' The Netherlands and Poland.
+#' The Data are fully described in Mossong J, Hens N, Jit M, Beutels P, Auranen
+#' K, Mikolajczyk R, et al. (2008) Social Contacts and Mixing Patterns Relevant
+#' to the Spread of Infectious Diseases. PLoS Med 5(3): e74.
+#'
+#' @format A list of two data frames:
+#' \describe{
+#'   \item{participants}{the study participant, with age, country, year and day
+#'   of the week (starting with 1 = Monday)}
+#'   \item{contacts}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{http://dx.doi.org/10.1371/journal.pmed.0050074}
+"POLYMOD"
