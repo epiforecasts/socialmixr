09f3308c1aa09ddef516bab0ecd37ae3fa963f83
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2017-11-07 00:03:59 +0100

    doc update

--- R/merge_surveys.r
+++ R/merge_surveys.r
@@ -1,6 +1,6 @@
 ##' Merge multiple surveys into one
 ##'
-##' @param surveys surveys to merge, given as a list of surveys compatible with the \code{survey} parameter in \code{\link{contat_mtarix}}.
+##' @param surveys surveys to merge, given as a list of surveys compatible with the \code{survey} parameter in \code{\link{contact_mtarix}}.
 ##' @return a survey in the correct format
 merge_surveys <- function(surveys)
 {
