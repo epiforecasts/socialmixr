fbe60c9954fbaa5453230e6230c9cb52db2b5a51
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2019-11-06 09:34:40 +0000

    remove message from test

--- tests/testthat/test-surveys.r
+++ tests/testthat/test-surveys.r
@@ -7,7 +7,7 @@ test_that("list of surveys is not empty",
 
 test_that("surveys can be downloaded",
 {
-  expect_true(class(suppressWarnings(get_survey("10.5281/zenodo.1059920"))) == "survey")
+  expect_true(class(suppressMessages(suppressWarnings(get_survey("10.5281/zenodo.1059920")))) == "survey")
 })
 
 test_that("surveys can be cited",
