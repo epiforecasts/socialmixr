c16852755d20110c23596c914aae5cffcbccbe35
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2019-12-11 12:17:12 +0000

    fix error with single-year agegroups

--- R/limits_to_agegroups.r
+++ R/limits_to_agegroups.r
@@ -11,13 +11,12 @@ limits_to_agegroups <- function(x, limits) {
     if (missing(limits)) limits <- unique(x)[order(unique(x))]
     limits <- limits[!is.na(limits)]
     agegroups <- c()
-    if (length(limits) > 1)
-    {
+    if (length(limits) > 1) {
         agegroups <- vapply(seq(1, length(limits) - 1), function(y) {
             if ((limits[y+1] - 1) > limits[y]) {
                 paste(limits[y], limits[y+1] - 1, sep = "-")
             } else {
-                limits[y]
+                paste(limits[y])
             }
         }, "")
     }
