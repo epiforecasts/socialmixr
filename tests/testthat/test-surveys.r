ceb5b0d900c4f788795809c1eecc0152cc83e336
lwillem <lander.willem@uantwerpen.be>
2020-12-14 11:51:03 +0100

    Made the test-survey on multiple DOI's more generic, since it is not based on internet connection, only function logic.

--- tests/testthat/test-surveys.r
+++ tests/testthat/test-surveys.r
@@ -19,14 +19,12 @@ test_that("surveys can be cited",
   expect_true(class(cite(polymod)) == "bibentry")
 })
 
-test_that("missing surveys can't' be cited",
+test_that("missing surveys can't be cited",
 {
   expect_error(cite.survey("bogus"), "not found")
 })
 
 test_that("multiple DOI's cannot be loaded",
   {
-    skip_if_no_zenodo()
-    skip_on_cran()
     expect_error(suppressMessages(suppressWarnings(get_survey(c("10.5281/zenodo.1059920","10.5281/zenodo.1059920")))))
   })
