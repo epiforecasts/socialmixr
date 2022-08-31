031eca4c8d71edc992a059449d95fa96ece8f6b2
Hugo Gruson <Bisaloo@users.noreply.github.com>
2022-08-31 13:09:09 +0200

    Skip tests hitting zenodo on CI

--- tests/testthat/test-surveys.r
+++ tests/testthat/test-surveys.r
@@ -3,12 +3,14 @@ context("Getting list of surveys")
 test_that("list of surveys is not empty", {
   skip_if_no_zenodo()
   skip_on_cran()
+  skip_on_ci()
   expect_true(nrow(list_surveys()) > 0)
 })
 
 test_that("surveys can be downloaded", {
   skip_if_no_zenodo()
   skip_on_cran()
+  skip_on_ci()
 
   s <- suppressMessages(suppressWarnings(get_survey("10.5281/zenodo.1059920")))
 
