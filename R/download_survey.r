5a69e94411f68204aeeaadb71a8fe1dac35eb49e
Hugo Gruson <Bisaloo@users.noreply.github.com>
2022-11-14 15:22:34 +0100

    Use if for stop() and remove else

--- R/download_survey.r
+++ R/download_survey.r
@@ -29,67 +29,67 @@ download_survey <- function(survey, dir = NULL) {
 
   if (is.doi) url <- paste0("https://doi.org/", survey) else url <- survey
 
-  if (is.url) {
-    temp_body <- GET(url, config = list(followlocation = TRUE))
-    if (status_code(temp_body) == 404) stop("DOI '", survey, "' not found")
-    if (http_error(temp_body)) {
-      stop(
-        "Could not fetch the resource. ",
-        "This could an issue with the website server or your own connection."
-      )
-    }
-
-    parsed_body <- content(temp_body, encoding = "UTF-8")
-    parsed_cite <- fromJSON(
-      xml_text(
-        xml_find_first(parsed_body, '//script[@type="application/ld+json"]')
-      )
-    )
-
-    reference <- list(
-      title = parsed_cite$name,
-      bibtype = "Misc",
-      author = parsed_cite$creator$name,
-      year = data.table::year(parsed_cite$datePublished)
-    )
-    if ("version" %in% names(parsed_cite)) {
-      reference[["note"]] <- paste("Version", parsed_cite$version)
-    }
-    reference[[ifelse(is.doi, "doi", "url")]] <- survey
-
-    data <- data.table(parsed_cite$distribution)
-    ## only download csv files
-    data <- data[encodingFormat == "csv"]
-    data[, file_name := tolower(basename(contentUrl))]
-
-    if (length(unique(data$file_name)) < length(data$file_name)) {
-      warning("Zenodo repository contains files with names that only differ by case. ",
-              "This will cause unpredictable behaviour on case-insensitive file systems. ",
-              "Please contact the authors to get this fixed.")
-      data <- data[!duplicated(file_name)]
-    }
-
-    if (is.null(dir)) {
-      dir <- tempdir()
-    }
-
-    message("Getting ", parsed_cite$name, ".")
-
-    lcs <- find_common_prefix(data$file_name)
-    reference_file_path <- file.path(dir, paste0(lcs, "reference.json"))
-    reference_json <- toJSON(reference)
-    write(reference_json, reference_file_path)
-
-    files <- c(reference_file_path, vapply(seq_len(nrow(data)), function(i) {
-      url <- data[i, ]$contentUrl
-      temp <- file.path(dir, data[i, ]$file_name)
-      message("Downloading ", url)
-      dl <- curl_download(url, temp)
-      return(temp)
-    }, ""))
-  } else {
+  if (!is.url) {
     stop("'survey' is not a DOI or URL.")
   }
+
+  temp_body <- GET(url, config = list(followlocation = TRUE))
+  if (status_code(temp_body) == 404) stop("DOI '", survey, "' not found")
+  if (http_error(temp_body)) {
+    stop(
+      "Could not fetch the resource. ",
+      "This could an issue with the website server or your own connection."
+    )
+  }
+
+  parsed_body <- content(temp_body, encoding = "UTF-8")
+  parsed_cite <- fromJSON(
+    xml_text(
+      xml_find_first(parsed_body, '//script[@type="application/ld+json"]')
+    )
+  )
+
+  reference <- list(
+    title = parsed_cite$name,
+    bibtype = "Misc",
+    author = parsed_cite$creator$name,
+    year = data.table::year(parsed_cite$datePublished)
+  )
+  if ("version" %in% names(parsed_cite)) {
+    reference[["note"]] <- paste("Version", parsed_cite$version)
+  }
+  reference[[ifelse(is.doi, "doi", "url")]] <- survey
+
+  data <- data.table(parsed_cite$distribution)
+  ## only download csv files
+  data <- data[encodingFormat == "csv"]
+  data[, file_name := tolower(basename(contentUrl))]
+
+  if (length(unique(data$file_name)) < length(data$file_name)) {
+    warning("Zenodo repository contains files with names that only differ by case. ",
+            "This will cause unpredictable behaviour on case-insensitive file systems. ",
+            "Please contact the authors to get this fixed.")
+    data <- data[!duplicated(file_name)]
+  }
+
+  if (is.null(dir)) {
+    dir <- tempdir()
+  }
+
+  message("Getting ", parsed_cite$name, ".")
+
+  lcs <- find_common_prefix(data$file_name)
+  reference_file_path <- file.path(dir, paste0(lcs, "reference.json"))
+  reference_json <- toJSON(reference)
+  write(reference_json, reference_file_path)
+
+  files <- c(reference_file_path, vapply(seq_len(nrow(data)), function(i) {
+    url <- data[i, ]$contentUrl
+    temp <- file.path(dir, data[i, ]$file_name)
+    message("Downloading ", url)
+    dl <- curl_download(url, temp)
+    return(temp)
+  }, ""))
 
   return(files)
 }
