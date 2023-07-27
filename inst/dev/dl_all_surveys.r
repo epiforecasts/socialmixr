bf98494ca18a8e1d9de9ecf674810f366417ccb7
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2023-07-27 11:38:15 +0100

    add script for downloading all surveys

---
+++ inst/dev/dl_all_surveys.r
@@ -0,0 +1,14 @@
+library("socialmixr")
+
+## list all surveys
+ls <- list_surveys()
+## download all surveys using the `url` column in the survey list and
+## save them in the `surveys` folder (which is created if it does not exist)
+survey_files <- purrr::map(ls$url, function(x) {
+  Sys.sleep(10)
+  download_survey(x, "surveys")
+})
+## name list elements according to url
+names(survey_files) <- ls$url
+## save list of survey files
+saveRDS(survey_files, here::here("surveys", "survey_files.rds"))
