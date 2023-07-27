6a7523b9d57b27075fa62cad6e3275c25acc5070
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2023-07-27 10:09:03 +0100

    add test

--- tests/testthat/test-surveys.r
+++ tests/testthat/test-surveys.r
@@ -22,13 +22,17 @@ test_that("surveys can be downloaded", {
 })
 
 test_that("surveys can be cited", {
-  expect_s3_class(cite(polymod), "bibentry")
+  expect_s3_class(get_citation(polymod), "bibentry")
 })
 
 test_that("missing surveys can't be cited", {
-  expect_error(cite.survey("bogus"), "URL")
+  expect_error(get_citation("bogus"), "URL")
 })
 
 test_that("multiple DOI's cannot be loaded", {
   expect_error(suppressMessages(suppressWarnings(get_survey(c("10.5281/zenodo.1059920", "10.5281/zenodo.1059920"))))) # nolint
+})
+
+test_that("deprecated functions are warned about", {
+  expect_warning(cite(polymod), "deprecated")
 })
