3d78f96593cf60753330adc4b8d4fde0d5d10ece
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2023-07-27 10:11:27 +0100

    update imports

--- R/download_survey.r
+++ R/download_survey.r
@@ -3,7 +3,7 @@
 #' @description Downloads survey data
 #' @param survey a URL (see [list_surveys()])
 #' @param dir a directory to save the files to; if not given, will save to a temporary directory
-#' @importFrom httr GET content status_code http_error user_agent
+#' @importFrom httr GET content status_code http_error config
 #' @importFrom jsonlite fromJSON toJSON
 #' @importFrom curl curl_download
 #' @importFrom utils read.csv packageVersion
