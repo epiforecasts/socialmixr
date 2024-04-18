99a70ef160809964940cd5a264eeda6c8bda970e
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2024-04-18 13:56:04 +0100

    create survey before downloading in script

--- inst/dev/dl_all_surveys.r
+++ inst/dev/dl_all_surveys.r
@@ -3,11 +3,13 @@ library("here")
 
 ## list all surveys
 ls <- list_surveys()
+
+dir.create(here("surveys"), showWarnings = FALSE)
 ## download all surveys using the `url` column in the survey list and
 ## save them in the `surveys` folder (which is created if it does not exist)
 survey_files <- purrr::map(ls$url, function(x) {
-  Sys.sleep(10)
   download_survey(x, "surveys")
+  Sys.sleep(10)
 })
 ## name list elements according to url
 names(survey_files) <- paste0(ls$title, " (", ls$url, ")")
