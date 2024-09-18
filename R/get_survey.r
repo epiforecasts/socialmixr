be2cc5df08f49f7d4c65657b273c5e7c79a736fd
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2024-09-18 12:21:30 +0100

    add note about caching/downloading

--- R/get_survey.r
+++ R/get_survey.r
@@ -1,6 +1,9 @@
 #' Get a survey, either from its Zenodo repository, a set of files, or a survey variable
+#
 #'
-#' @description Downloads survey data, or extracts them from files, and returns a clean data set.
+#' Downloads survey data, or extracts them from files, and returns a clean data set. If a survey URL is accessed multiple times, the data will be cached (unless `clear_cache` is set to `TRUE`) to avoid repeated downloads.
+#'
+#' If survey objects are used repeatedly the downloaded files can be saved and reloaded between sessions then survey objects can be saved/loaded using [base::saveRDS()] and [base::readRDS()], or via the individual survey files that can be downloaded using [download_survey()] and subsequently loaded using [load_survey()].
 #' @param clear_cache logical, whether to clear the cache before downloading the survey; by default, the cache is not cleared and so multiple calls of this function to access the same survey will not result in repeated downloads
 #' @importFrom memoise memoise
 #' @inheritParams .get_survey
