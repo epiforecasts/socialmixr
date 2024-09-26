0b4aae00008884a9b74c96d0b58731e959c3d832
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2024-09-26 20:44:51 +0100

    don't plot in tests

--- tests/testthat/test-plot.r
+++ tests/testthat/test-plot.r
@@ -5,21 +5,31 @@ suppressWarnings({
 })
 
 test_that("contact matrix can be plotted", {
+  pdf(file = NULL)
   expect_no_error(matrix_plot(dta$matrix))
+  dev.off()
 })
 
 test_that("contact matrix per capita can be plotted", {
+  pdf(file = NULL)
   expect_no_error(matrix_plot(dta$matrix.per.capita))
+  dev.off()
 })
 
 test_that("contact matrix can be plotted with different color palette", {
+  pdf(file = NULL)
   expect_no_error(matrix_plot(dta$matrix, color.palette = rainbow))
+  dev.off()
 })
 
 test_that("contact matrix can be plotted with ad-hoc min and max values for the legend", {
+  pdf(file = NULL)
   expect_no_error(matrix_plot(dta$matrix, min.legend = 4, max.legend = 40))
+  dev.off()
 })
 
 test_that("contact matrix can be plotted with (ad-hoc) min and max values for the legend", {
+  pdf(file = NULL)
   expect_no_error(matrix_plot(dta$matrix, min.legend = 4, max.legend = 40))
+  dev.off()
 })
