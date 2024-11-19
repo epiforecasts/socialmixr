5a9143f4b642b416d0cf853443f2610379cebd56
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2024-11-19 11:18:24 +0000

    copy survey when getting

--- R/get_survey.R
+++ R/get_survey.R
@@ -26,10 +26,11 @@ get_survey <- function(survey, clear_cache = FALSE, ...) {
 #' @autoglobal
 #' @param survey a DOI or url to get the survey from, or a [survey()] object (in which case only cleaning is done).
 #' @param ... options for [clean()], which is called at the end of this
+#' @importFrom data.table copy
 #' @keywords internal
 .get_survey <- function(survey, ...) {
   if (inherits(survey, "contact_survey")) {
-    new_survey <- survey
+    new_survey <- copy(survey)
   } else {
     if (is.character(survey)) {
       files <- download_survey(survey)
