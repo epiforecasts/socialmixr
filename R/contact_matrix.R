2b321933ac5611e7569545e5d4bae76f77d8a19f
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2024-11-19 21:25:38 +0000

    move filtering to function

--- R/contact_matrix.R
+++ R/contact_matrix.R
@@ -39,6 +39,12 @@
 contact_matrix <- function(survey, countries = NULL, survey.pop, age.limits, filter, counts = FALSE, symmetric = FALSE, split = FALSE, sample.participants = FALSE, estimated.participant.age = c("mean", "sample", "missing"), estimated.contact.age = c("mean", "sample", "missing"), missing.participant.age = c("remove", "keep"), missing.contact.age = c("remove", "sample", "keep", "ignore"), weights = NULL, weigh.dayofweek = FALSE, weigh.age = FALSE, weight.threshold = NA, symmetric.norm.threshold = 2, sample.all.age.groups = FALSE, return.part.weights = FALSE, return.demography = NA, per.capita = FALSE, ...) {
   ## === check arguments and define variables
 
+  lifecycle::deprecate_warn(
+    "1.0.0",
+    "contact_matrix(filter)",
+    "filter_survey()"
+  )
+
   dot.args <- list(...)
   unknown.args <- setdiff(names(dot.args), union(names(formals(check.contact_survey)), names(formals(pop_age))))
   if (length(unknown.args) > 0) {
@@ -223,6 +229,7 @@ contact_matrix <- function(survey, countries = NULL, survey.pop, age.limits, fil
   }
 
   ## === check if any filters have been requested
+  ## deprecated functionality to be removed
   if (!missing(filter)) {
     missing_columns <- list()
     for (table in c("participants", "contacts")) {
