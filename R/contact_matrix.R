7f966f5197ed1d991fd5e4b283b852887b5b90a3
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2024-12-05 01:22:13 +0000

    move assertion down

--- R/contact_matrix.R
+++ R/contact_matrix.R
@@ -38,9 +38,8 @@
 #' contact_matrix(polymod, countries = "United Kingdom", age.limits = c(0, 1, 5, 15))
 #' @author Sebastian Funk
 contact_matrix <- function(survey, countries = NULL, survey.pop, age.limits, filter, counts = FALSE, symmetric = FALSE, split = FALSE, sample.participants = FALSE, estimated.participant.age = c("mean", "sample", "missing"), estimated.contact.age = c("mean", "sample", "missing"), missing.participant.age = c("remove", "keep"), missing.contact.age = c("remove", "sample", "keep", "ignore"), weights = NULL, weigh.dayofweek = FALSE, weigh.age = FALSE, weight.threshold = NA, symmetric.norm.threshold = 2, sample.all.age.groups = FALSE, return.part.weights = FALSE, return.demography = NA, per.capita = FALSE, ...) {
-  assert_class(survey, "contact_survey")
   ## === check arguments and define variables
-
+  assert_class(survey, "contact_survey")
   lifecycle::deprecate_warn(
     "1.0.0",
     "contact_matrix(filter)",
