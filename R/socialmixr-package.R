2d6821fb000a27d688082b763c16d7eacd0ea1df
njtierney <nicholas.tierney@gmail.com>
2025-09-03 10:00:32 +1000

    use_lifecycle()

--- R/socialmixr-package.R
+++ R/socialmixr-package.R
@@ -1,2 +1,6 @@
 .socialmixr.env <- new.env(parent = emptyenv())
 assign("cached_functions", list(), envir = .socialmixr.env)
+## usethis namespace: start
+#' @importFrom lifecycle deprecated
+## usethis namespace: end
+NULL
