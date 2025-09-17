d0024037e3ef099601af0fb3def0d8bd8d403c6a
Nicholas Tierney <njtierney@users.noreply.github.com>
2025-09-17 18:15:30 +1000

    soft deprecate `download_survey()`, `get_citation()` and `list_surveys()` and point to {contactsurveys} (#209)
    * use_lifecycle()
    
    * Soft-deprecate `download_survey()`, `get_citation()` and `list_surveys()` with the lifecycle package.
    
    * remove deprecate_warn from Imports
    
    * deprecated warning --> deprecation warning
    
    * namespace contactsurveys in examples
    
    * ensure get_citation() refers to survey and not x in the body of the code
    
    * add check for NA value in survey in download_survey()
    
    * Add deprecation for get_survey()
    
    * add tryCatch to list_surveys()
    
    * fix linting error
    
    * soft deprecate survey_countries()
    
    * Update R/download_survey.R
    
    Co-authored-by: Sebastian Funk <sebastian.funk@lshtm.ac.uk>
    
    * Update documentation
    
    ---------
    
    Co-authored-by: Sebastian Funk <sebastian.funk@lshtm.ac.uk>

--- tests/testthat/test-countries.r
+++ tests/testthat/test-countries.r
@@ -1,6 +1,13 @@
 test_that("list of countries is not empty", {
   expect_gt(length(wpp_countries()), 0)
+  withr::local_options(lifecycle_verbosity = "quiet")
   expect_gt(length(survey_countries(polymod)), 0)
+})
+
+test_that("survey_countries() sends deprecation warning", {
+  expect_snapshot_warning(
+    survey_countries(polymod)
+  )
 })
 
 test_that("population data for 2015-2020 can be loaded", {
