78579ad709800065677d7a21c27e1d990bb6c968
Lloyd Chapman <22941537+LloydChapman@users.noreply.github.com>
2025-12-18 14:17:06 +0000

    Change indentation in tests/testthat/test-clean.R
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- tests/testthat/test-clean.R
+++ tests/testthat/test-clean.R
@@ -6,8 +6,8 @@ test_that("clean() creates est_min/est_max for age ranges", {
 
   cleaned <- clean(survey)
 
-    expect_true("part_age_est_min" %in% names(cleaned$participants))
-    expect_true("part_age_est_max" %in% names(cleaned$participants))
-    expect_equal(cleaned$participants$part_age_est_min[1], 20)
-    expect_equal(cleaned$participants$part_age_est_max[1], 30)
+  expect_true("part_age_est_min" %in% names(cleaned$participants))
+  expect_true("part_age_est_max" %in% names(cleaned$participants))
+  expect_equal(cleaned$participants$part_age_est_min[1], 20)
+  expect_equal(cleaned$participants$part_age_est_max[1], 30)
 })
