c852232b3c59636c258cae4d8c4b8de02aa99cba
Lloyd Chapman <lacchapman@gmail.com>
2025-12-18 14:05:42 +0000

    Add regression test for clean()

---
+++ tests/testthat/test-clean.R
@@ -0,0 +1,13 @@
+test_that("clean() creates est_min/est_max for age ranges", {
+    survey <- polymod
+    # Replace exact ages with ranges
+    survey$participants$part_age <- "20-30"
+    survey$participants$part_age_exact <- NULL
+
+    cleaned <- clean(survey)
+
+    expect_true("part_age_est_min" %in% names(cleaned$participants))
+    expect_true("part_age_est_max" %in% names(cleaned$participants))
+    expect_equal(cleaned$participants$part_age_est_min[1], 20)
+    expect_equal(cleaned$participants$part_age_est_max[1], 30)
+})
